807c42974578c6ce73395accfed418a110aad132
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-07-08 13:28:02 +0700

    Add unitest

--- java/src/Part4/Main.java
+++ java/src/Part4/Main.java
@@ -1,4 +1,14 @@
 package Part4;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Main {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        // assert statements
+        Assert.assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
+        Assert.assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+        Assert.assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
 }
